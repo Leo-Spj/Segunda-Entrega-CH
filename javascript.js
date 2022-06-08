ce5b18a16b14a6f233c171a12c198ca1f2d261fa
Leo <85245534+Leo-Spj@users.noreply.github.com>
2022-06-08 14:34:16 -0500

    Funciona correctamente el boton de agregar y remover. tambien funciona correctamente la actualizacion del array

--- javascript.js
+++ javascript.js
@@ -11,8 +11,7 @@ class alumnos_lista {
 const alumno = [ ];
 
 // alumnos de ejemplo dentro del array
-alumno.push(new alumnos_lista (10234454, "leo", "lala"));
-alumno.push(new alumnos_lista (1113423, "vale", "lulu"));
+
 
 
 
@@ -23,128 +22,68 @@ const input_id = document.querySelector(".input_id");
 
 const boton_formulario = document.querySelector(".boton_formulario");
 
-const tabla = document.querySelector(".tabla")
+const tabla = document.getElementById("tabla")
+
+const alumnos_impresos = document.getElementById("alumnos_impresos");
 
 
-/**
-input_nombre.oninput = function(){
-console.log(input_nombre.value)
-}
-input_apellido.oninput = function(){
-console.log(input_apellido.value)
-}
-input_id.oninput = function(){
-console.log(input_id.value)
-}
-*/
+
 
 
 boton_formulario.onclick = function(){
     
-
     let escuchar_nombre = document.getElementById("input_nombre").value
     let input_apellido = document.getElementById("input_apellido").value
     let input_id = document.getElementById("input_id").value
 
+    numeroId = parseFloat(input_id);
 
-    console.log(escuchar_nombre)
-    console.log(input_apellido)
-    console.log(input_id)
-
+    alumno.push(new alumnos_lista(numeroId, escuchar_nombre, input_apellido));
 
 
-}
+    // Borrar:
+    document.getElementById("input_nombre").value = "";
+    document.getElementById("input_apellido").value = "";
+    document.getElementById("input_id").value = "";
 
-
-// Para escuchar los nuevos ingresos de usuarios se podria hacer escuchar al boton de Registrar y con eso actualizar el ciclo For de aqui abajo
-
-for (let i=0; i<alumno.length; i++){
-
-    datos_alumno = alumno[i];
-
-    agregar_alumno_tabla(datos_alumno, i);
-
-
-    
-}
-
-function agregar_alumno_tabla(datos_alumno, i){
-    
-    datos_alumno
+    // meter htmnl:
 
     let tr = document.createElement("tr");
     
-    let identificacion = alumno[i].id;
+    // obtener la longitud del array
+    let long = alumno.length;
+    let leer_ultimo_agregado = long-1
 
-    tr.setAttribute("id", identificacion+"-id")
 
-    tabla.appendChild( tr )
+    tr.setAttribute("id", alumno[leer_ultimo_agregado].id+"-id")
 
-    
+    alumnos_impresos.appendChild( tr )
+
     tr.innerHTML = `
-    <td>  ${alumno[i].id}  </td>
-    <td>  ${alumno[i].nombre}  </td>
-    <td>  ${alumno[i].apellido}  </td>
-    
-    <td>
-    <button class="boton_lista" id="${alumno[i].id}">X</button>
-    </td>
-    ` ;
-    
-    let eliminar_alumno = document.getElementById(identificacion);
-    
-    let quitar_html_alumno = document.getElementById(identificacion+"-id")
-
-    eliminar_alumno.onclick = function(){
+        <td>  ${alumno[leer_ultimo_agregado].id}  </td>
+        <td>  ${alumno[leer_ultimo_agregado].nombre}  </td>
+        <td>  ${alumno[leer_ultimo_agregado].apellido}  </td>
         
+        <td>
+        <button class="boton_lista" id="${alumno[leer_ultimo_agregado].id}">X</button>
+        </td>
+        ` ;
 
-        posicion = alumno.findIndex(x => x.id === identificacion)
+        let identificacion = alumno[leer_ultimo_agregado].id;
 
-        alumno.splice(posicion, 1)
-
-        quitar_html_alumno.remove()
-    }
+        let eliminar_alumno = document.getElementById(identificacion);
+        
+        let quitar_html_alumno = document.getElementById(identificacion+"-id")
     
-   
+        eliminar_alumno.onclick = function(){
+            
+    
+            posicion = alumno.findIndex(x => x.id === identificacion)
+    
+            alumno.splice(posicion, 1)
+    
+            quitar_html_alumno.remove()
+        }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
